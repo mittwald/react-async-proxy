b6f265dfea4ee2ce5cd33847088d0943abb0b396
Marco Falkenberg <m.falkenberg@mittwald.de>
2025-10-23 09:31:29 +0200

    fix: fix issues with objects as function args

--- packages/react-async-proxy/src/getModelQueryKey.ts
+++ packages/react-async-proxy/src/getModelQueryKey.ts
@@ -1,22 +1,39 @@
 import is from "@sindresorhus/is";
 import { modelIdentifiers } from "./modelIdentifier";
+import { hash } from "object-code";
+
+const getObjectName = (something: unknown): string | null => {
+  const isObject = is.object(something);
+  const isClass = is.class(something);
+  const isFunction = is.function(something);
+
+  return isFunction
+    ? something.name
+    : isClass || isObject
+      ? something.constructor.name
+      : null;
+};
+
+const getArgKey = (arg: unknown) => {
+  if (is.plainObject(arg)) {
+    /**
+     * Opt-out of TanStack Query's default key generation via JSON-Stringify
+     * because it does not support circular references
+     */
+    const objectName = getObjectName(arg);
+    const objectHash = hash(arg);
+    return `${objectName}(${objectHash})`;
+  }
+  return arg;
+};
 
 export const getModelQueryKey = (
   model: unknown,
   propName: string,
   ...args: unknown[]
 ): unknown[] => {
-  const isObject = is.object(model);
-  const isClass = is.class(model);
-  const isFunction = is.function(model);
-
-  const staticModelKey = isFunction
-    ? model.name
-    : isClass || isObject
-      ? model.constructor.name
-      : null;
-
-  const thisModelIdentifiers = modelIdentifiers.map((fn) => fn(model));
-
-  return [staticModelKey, ...thisModelIdentifiers, propName, ...args];
+  const objectName = getObjectName(model);
+  const currentModelIdentifiers = modelIdentifiers.map((fn) => fn(model));
+  const argKeys = args.map(getArgKey);
+  return [objectName, ...currentModelIdentifiers, propName, ...argKeys];
 };
