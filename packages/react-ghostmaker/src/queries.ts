0c1e273eb0c0427946f26d92b13e1ba513ff3080
Marco Falkenberg <m.falkenberg@mittwald.de>
2025-11-07 09:38:54 +0100

    feat: prepend object name in query key for better debugging

--- packages/react-ghostmaker/src/queries.ts
+++ packages/react-ghostmaker/src/queries.ts
@@ -3,20 +3,24 @@ import { modelIdentifiers } from "./modelIdentifier";
 import type { GhostChain, GhostChainItem, QueryKey } from "./types";
 import { hashObject } from "./hash";
 
+const getObjectName = (something: unknown) => {
+  const isObject = is.object(something);
+  const isClass = is.class(something);
+  const isFunction = is.function(something);
+
+  return isFunction
+    ? something.name
+    : isClass || isObject
+      ? something.constructor.name
+      : "unknown";
+};
+
 const getTargetQueryKey = (something: unknown): string => {
   if (is.primitive(something)) {
     return String(something);
   }
 
-  const isObject = is.object(something);
-  const isClass = is.class(something);
-  const isFunction = is.function(something);
-
-  const objectName = isFunction
-    ? something.name
-    : isClass || isObject
-      ? something.constructor.name
-      : "unknown";
+  const objectName = getObjectName(something);
 
   const thisModelIdentifiers = modelIdentifiers
     .map((fn) => fn(something))
@@ -34,7 +38,7 @@ const getArgKey = (arg: unknown) => {
   if (is.primitive(arg)) {
     return String(arg);
   }
-  return hashObject(arg).toString();
+  return `${getObjectName(arg)}(hash:${hashObject(arg)})`;
 };
 
 export const queries = {
