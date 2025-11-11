a186aef16829cde2e1fa3807c4199f10959a5050
Marco Falkenberg <m.falkenberg@mittwald.de>
2025-11-11 08:23:28 +0100

    fix: export deprecated registerModelIdentifier

--- packages/react-ghostmaker/src/index.ts
+++ packages/react-ghostmaker/src/index.ts
@@ -1,4 +1,5 @@
 export { makeGhost } from "./makeGhost";
+export { registerModelIdentifier } from "./modelIdentifier";
 export { type ReactGhost, UseGhostReturn } from "./types";
 export { getQueryContext, forwardQueryContext } from "./context";
 export { invalidateGhosts } from "./invalidate";
