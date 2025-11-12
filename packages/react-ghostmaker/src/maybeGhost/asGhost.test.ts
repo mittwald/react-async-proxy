b9ab828c907e57532909d52050c88162bf2ee708
Marco Falkenberg <m.falkenberg@mittwald.de>
2025-11-12 09:53:12 +0100

    fix: fix undefined should not be converted to Ghost

---
+++ packages/react-ghostmaker/src/maybeGhost/asGhost.test.ts
@@ -0,0 +1,59 @@
+import { asGhostProps } from "../maybeGhost";
+import { isReactGhost } from "../types";
+import { expect, test } from "vitest";
+
+interface Props {
+  first: string;
+  second?: string;
+  third: string | undefined;
+}
+
+test("makes Ghosts of all props", () => {
+  const ghostProps = asGhostProps<Props>({
+    first: "hello",
+    second: "world",
+    third: "!",
+  });
+
+  expect(ghostProps.firstGhost).toSatisfy(isReactGhost);
+  expect(ghostProps.secondGhost).toSatisfy(isReactGhost);
+  expect(ghostProps.thirdGhost).toSatisfy(isReactGhost);
+});
+
+test("does not make Ghosts of explicit undefined", () => {
+  const ghostProps = asGhostProps<Props>({
+    first: "hello",
+    second: undefined,
+    third: "!",
+  });
+
+  expect(ghostProps.firstGhost).toSatisfy(isReactGhost);
+  expect(ghostProps.secondGhost).toBeUndefined();
+  expect(ghostProps.thirdGhost).toSatisfy(isReactGhost);
+});
+
+test("does not make Ghosts of not given props", () => {
+  const ghostProps = asGhostProps<Props>({
+    first: "hello",
+    third: "!",
+  });
+
+  expect(ghostProps.firstGhost).toSatisfy(isReactGhost);
+  expect(ghostProps.secondGhost).toBeUndefined();
+  expect(ghostProps.thirdGhost).toSatisfy(isReactGhost);
+});
+
+test("does not make Ghosts of excluded props", () => {
+  const ghostProps = asGhostProps<Props>(
+    {
+      first: "hello",
+      second: "world",
+      third: "!",
+    },
+    ["first"],
+  );
+
+  expect(ghostProps.firstGhost).toSatisfy(isReactGhost);
+  expect(ghostProps.secondGhost).not.toSatisfy(isReactGhost);
+  expect(ghostProps.thirdGhost).not.toSatisfy(isReactGhost);
+});
